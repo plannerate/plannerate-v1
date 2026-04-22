e7d82d26f4178181b1b891124153ef1c76ef725c
Claudio Campos <callcocam@gmail.com>
2026-04-21 23:38:17 -0300

    feat: add Role management controllers, requests, and policy for landlord functionality

---
+++ app/Http/Controllers/Landlord/TenantUserAccessController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Landlord;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TenantUserAccessController extends Controller
+{
+    //
+}
