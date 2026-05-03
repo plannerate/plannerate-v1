6c725d9da2530eb93cfad5cc97a6165f0be7b6a6
Claudio Campos <callcocam@gmail.com>
2026-05-03 13:47:11 -0300

    fix(config): update landlord domain to use localhost in .env files and app configuration

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'landlord_domain' => env('LANDLORD_DOMAIN', 'siga.dev.br'),
+    'landlord_domain' => env('LANDLORD_DOMAIN', 'localhost'),
 
     /*
     |--------------------------------------------------------------------------
